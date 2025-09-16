aee6d7cc45bdae81cb1f26e7797bc1edacd8d597
Juan8516 <juan.ind85@gmail.com>
2025-09-16 13:45:16 -0500

    Arquitectura aplicacion
    Instalacion de paquetes AR Core y AR Fundation
    
    Creacion de script GameManager
    
    Se prueba aplicacion sin errores

---
+++ 01ProyectoPlanosAR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action OnManiMenu;
+    public event Action OnItemsMenu;
+    public event Action OnARPosition;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance!=null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MainMenu();
+    }
+
+    public void MainMenu()
+    {
+        OnManiMenu?.Invoke();
+        Debug.Log("Main Menu Activated");
+    }
+
+    public void ItemsMenu()
+    {
+        OnItemsMenu?.Invoke();
+        Debug.Log("Items Menu Activated");
+    }
+
+    public void ARPosition()
+    {
+        OnARPosition?.Invoke();
+        Debug.Log("AR Position Activated");
+    }
+
+    public void CloseAPP()
+    {
+        Application.Quit();
+        Debug.Log("Application Closed");
+    }
+
+}
